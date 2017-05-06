7019fce2afe1ebed927b5b11c749cac6217597f8
Sam Okoro <s.okoro07@gmail.com>
2017-05-06 17:06:11 -0500

    First Commit

---
+++ index.ios.js
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+
+} from 'react-native';
+
+import Pull from './src/ui'
+AppRegistry.registerComponent('Pull', () => Pull);
